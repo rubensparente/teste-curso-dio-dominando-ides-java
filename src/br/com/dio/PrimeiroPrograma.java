56ea8cefde92419e336ea4f2854e7529ae098f99
Rubens Parente <rubensparente@yahoo.com.br>
2023-02-19 10:54:40 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		
+	}
+
+	
+}
